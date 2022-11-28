3d0d62b600b4763016ad04cc0a8f61acfaf9845e
Dipesh-J <dipeshjoshi015@gmail.com>
2022-11-28 15:05:30 +0530

    structure created

---
+++ src/models/bookModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    excerpt: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    ISBN: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    subcategory: {
+      type: String,
+      required: true,
+    },
+    reviews: {
+      type: Number,
+      default: 0,
+    },
+    deletedAt: {
+      type: String,
+      default: null,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    releasedAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('book',bookSchema)
---
+++ src/models/reviewModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const reviewerSchema = new mongoose.Schema({
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "book",
+  },
+  reviewedBy: {
+    type: String,
+    required: true,
+    // default 'Guest',
+    // value:
+  },
+  reviewedAt: {
+    type: Date,
+    required: true,
+  },
+  rating: {
+    type: Number, //min 1, max 5, mandatory
+  },
+  review: {
+    type: String,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("reviewer", reviewerSchema);
