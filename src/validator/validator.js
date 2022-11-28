7f3f45706ef2aa5d308fff1fa4dadce323cc5311
pranaynaidu04 <pranaynaidu482@gmail.com>
2022-11-28 16:31:47 +0530

    done with user api

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,1 +1,31 @@
-// validator
+//===============================>> String Validation <<==================================>>>
+const isValid = function (value) {
+    return (typeof value === "string" &&  value.trim().length > 0 && value.match(/^[\D]+$/)) 
+};
+
+  //================================>> Email Validation <<===================================>>>
+
+const isValidEmail = function (email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+//===============================>> Mobile Validation <<====================================>>>
+
+const isValidPhone = function (phone) {
+    return /^([+]\d{2})?\d{10}$/.test(phone);
+}
+
+
+//===============================>> Password Validation <<====================================>>>
+
+const isValidPassword = function (pwd) {
+    let passwordRegex =
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/;
+  
+    if (passwordRegex.test(pwd)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  module.exports = { isValid, isValidEmail, isValidPhone, isValidPassword };
